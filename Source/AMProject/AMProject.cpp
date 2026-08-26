32a2d1b82244d3d1590502ad3065cbe505aa3088
taene <rlaxodus3445@naver.com>
2026-08-26 13:29:03 +0900

    Initial commit: Base project setup

---
+++ Source/AMProject/AMProject.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "AMProject.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, AMProject, "AMProject" );
